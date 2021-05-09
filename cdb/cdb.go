816ac213653303d567500b6233541b3944aba1b6
tokizuoh <k_sakai@yumemi.co.jp>
2021-05-09 09:50:45 +0900

    move processing cdb package from main

--- cdb/cdb.go
+++ cdb/cdb.go
@@ -1,8 +1,55 @@
 package cdb
 
-import "log"
+import (
+	"database/sql"
+	"log"
 
-// TODO: データベースの処理を追加する
-func A() {
-	log.Println("Hello, World!")
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type word struct {
+	wordid int
+	lang   string
+	lemma  string
+	pron   string
+	pos    int
+}
+
+// [TODO]: 関数の機能が複数（クエリ実行、構造体へのパース）なので分けたほうが良さそう
+// getJapaneseWords executes a query that returns Japanese only rows.
+func getJapaneseWords(db *sql.DB) ([]word, error) {
+	rows, err := db.Query("select * from word where lang='jpn'")
+	if err != nil {
+		return []word{}, err
+	}
+	defer rows.Close()
+
+	var words = []word{}
+	for rows.Next() {
+		var w word
+		rows.Scan(&w.wordid, &w.lang, &w.lemma, &w.pron, &w.pos)
+		words = append(words, w)
+	}
+	return words, nil
+}
+
+func GenerateCustomDB() error {
+	var db *sql.DB
+	db, err := sql.Open("sqlite3", "./wnjpn.db")
+	if err != nil {
+		return err
+	}
+	// Closeしなかったらどうなる？
+	defer db.Close()
+
+	words, err := getJapaneseWords(db)
+	if err != nil {
+		return err
+	}
+
+	for _, word := range words {
+		log.Println(word)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,57 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
 	"cdb"
-
-	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
-type word struct {
-	wordid int
-	lang   string
-	lemma  string
-	pron   string
-	pos    int
-}
-
-// [TODO]: 関数の機能が複数（クエリ実行、構造体へのパース）なので分けたほうが良さそう
-// getJapaneseWords executes a query that returns Japanese only rows.
-func getJapaneseWords(db *sql.DB) ([]word, error) {
-	rows, err := db.Query("select * from word where lang='jpn'")
-	if err != nil {
-		return []word{}, err
-	}
-	defer rows.Close()
-
-	var words = []word{}
-	for rows.Next() {
-		var w word
-		rows.Scan(&w.wordid, &w.lang, &w.lemma, &w.pron, &w.pos)
-		words = append(words, w)
-	}
-	return words, nil
-}
-
 func main() {
-	cdb.A()
-	return
-	var db *sql.DB
-	db, err := sql.Open("sqlite3", "./wnjpn.db")
-	if err != nil {
+	if err := cdb.GenerateCustomDB(); err != nil {
 		log.Fatal(err)
-	}
-	// Closeしなかったらどうなる？
-	defer db.Close()
-
-	words, err := getJapaneseWords(db)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, word := range words {
-		log.Println(word)
 	}
 }
